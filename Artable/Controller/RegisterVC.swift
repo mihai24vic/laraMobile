1447eee049e0076da27b47ff304bb06346dbddfc
Roxana Bratosin <roxana.bratosin25@gmail.com>
2019-05-30 01:35:20 +0300

    user details

--- Artable/Controller/RegisterVC.swift
+++ Artable/Controller/RegisterVC.swift
@@ -9,10 +9,16 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import Alamofire
 
 class RegisterVC : UIViewController{
-    //outlets
     
+     let URL_USER_REGISTER = "https://stackwish.com/v1/register.php"
+    
+    //the defaultvalues to store user data
+    let defaultValues = UserDefaults.standard
+    
+    //outlets
     @IBOutlet weak var usernameText: UITextField!
     @IBOutlet weak var emailText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
@@ -57,6 +63,10 @@ class RegisterVC : UIViewController{
     }
     
     
+    @IBAction func backToStoreFromRegister(_ sender: Any) {
+        presentMainController()
+    }
+    
     
     @IBAction func registerClicked(_ sender: Any) {
         
@@ -65,28 +75,56 @@ class RegisterVC : UIViewController{
           let pass = passwordText.text, pass.isNotEmpty,
         let pass2 = confirmPassword.text, pass2.isNotEmpty else {return}
         
+        let parameters: Parameters=[
+            "username":usernameText.text!,
+            "password":passwordText.text!,
+            "email":emailText.text!
+        ]
+        
+        
         activityIndicator.isHidden = false
         activityIndicator.startAnimating()
         
-        Auth.auth().createUser(withEmail: email, password: pass) { (authResult, error) in
-            // ...
-            
-            if let error = error {
-                debugPrint(error)
-                return
-            }
-            
-            
-            self.activityIndicator.stopAnimating()
-            self.dismiss(animated: true, completion: nil)
+        //making a post request
+        Alamofire.request(URL_USER_REGISTER, method: .post, parameters: parameters).responseJSON
+            {
+                response in
+                //printing response
+                print("*************  \(response)")
+                
+                //getting the json value from the server
+                if let result = response.result.value {
+                    let jsonData = result as! NSDictionary
+                    
+                    //if there is no error
+                    if(!(jsonData.value(forKey: "error") as! Bool))
+                    {
+                        
+                        //getting the user from response
+                        let user = jsonData.value(forKey: "user") as! NSDictionary
+                        
+                        //getting user values
+                        let userId = user.value(forKey: "id") as! Int
+                        let userName = user.value(forKey: "username") as! String
+                        let userEmail = user.value(forKey: "email") as! String
+                        
+                        //saving user values to defaults
+                        self.defaultValues.set(userId, forKey: "userid")
+                        self.defaultValues.set(userName, forKey: "username")
+                        self.defaultValues.set(userEmail, forKey: "useremail")
+                        
+                        self.presentMainController()
+                        
+                    }else{
+                        //error message in case of invalid credential
+                        //self.labelError.text = "Error"
+                    }
+                }
         }
-    }
-    
-    
-    @IBAction func backToStoreFromRegister(_ sender: Any) {
-        presentMainController()
-    }
-    
+//            self.activityIndicator.stopAnimating()
+//            self.dismiss(animated: true, completion: nil)
+        }
+
     fileprivate func presentMainController(){
         let storyBoard = UIStoryboard(name: "Main", bundle: nil)
         let controller = storyBoard.instantiateViewController(withIdentifier: "tabView")
