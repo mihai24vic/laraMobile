267d3a7dac9288bda7ee636b26e5039f68774490
Mihai Bocanu <mihai24ivc@gmail.com>
2019-06-26 00:48:47 +0300

    working log in

--- Artable/Controller/LoginVC.swift
+++ Artable/Controller/LoginVC.swift
@@ -8,12 +8,22 @@
 
 import UIKit
 import Firebase
+import Alamofire
 
 
 class LoginVC: UIViewController {
-
+    
+    //The login script url make sure to write the ip instead of localhost
+    //you can get the ip using ifconfig command in terminal
+    let URL_USER_LOGIN = "https://stackwish.com/v1/login.php"
+    
+    //the defaultvalues to store user data
+    let defaultValues = UserDefaults.standard
+    
     @IBOutlet weak var emailTxt: UITextField!
     @IBOutlet weak var passwordTxt: UITextField!
+    
+    @IBOutlet weak var labelError: UITextField!
     
     @IBOutlet weak var acitivityIndicator: UIActivityIndicatorView!
     @IBAction func forgotPassClicked(_ sender: Any) {
@@ -29,22 +39,76 @@ class LoginVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("*************  \(String(describing: defaultValues.string(forKey: "username")))")
+        
+        if defaultValues.string(forKey: "username") != nil{
+            print("9999999999")
+            print(defaultValues.string(forKey: "username")!)
+            
+            
+            let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+            let controller = storyBoard.instantiateViewController(withIdentifier: "LoggedUser")
+            present(controller, animated: true, completion: nil)
+            
+        }
     }
     
     @IBAction func loginClicked(_ sender: Any) {
-        guard let email = emailTxt.text , email.isNotEmpty,
-            let password = passwordTxt.text , password.isNotEmpty else {return}
+        //        guard let email = emailTxt.text , email.isNotEmpty,
+        //            let password = passwordTxt.text , password.isNotEmpty else {return}
+        //
+        //        acitivityIndicator.startAnimating()
+        //        Auth.auth().signIn(withEmail: email, password: password) {(user, error) in
+        //            if let error = error {
+        //                debugPrint(error)
+        //                self.acitivityIndicator.stopAnimating()
+        //                return
+        //            }
+        //            self.acitivityIndicator.stopAnimating()
+        //            self.dismiss(animated: true, completion: nil)
+        //
+        //        }
+        let parameters: Parameters=[
+            "username":emailTxt.text!,
+            "password":passwordTxt.text!
+        ]
         
-        acitivityIndicator.startAnimating()
-        Auth.auth().signIn(withEmail: email, password: password) {(user, error) in
-            if let error = error {
-                debugPrint(error)
-                self.acitivityIndicator.stopAnimating()
-                return
-            }
-            self.acitivityIndicator.stopAnimating()
-            self.dismiss(animated: true, completion: nil)
-            
+        //making a post request
+        Alamofire.request(URL_USER_LOGIN, method: .post, parameters: parameters).responseJSON
+            {
+                response in
+                //printing response
+                print("*************  \(response)")
+                
+                //getting the json value from the server
+                if let result = response.result.value {
+                    let jsonData = result as! NSDictionary
+                    
+                    //if there is no error
+                    if(!(jsonData.value(forKey: "error") as! Bool)){
+                        self.labelError.text = ""
+                        
+                        //getting the user from response
+                        let user = jsonData.value(forKey: "user") as! NSDictionary
+                        
+                        //getting user values
+                        let userId = user.value(forKey: "id") as! Int
+                        let userName = user.value(forKey: "username") as! String
+                        let userEmail = user.value(forKey: "email") as! String
+                        
+                        //saving user values to defaults
+                        self.defaultValues.set(userId, forKey: "userid")
+                        self.defaultValues.set(userName, forKey: "username")
+                        self.defaultValues.set(userEmail, forKey: "useremail")
+                        
+                        self.presentMainController()
+                        
+                    }else{
+                        //error message in case of invalid credential
+                        self.labelError.text = "Invalid username or password"
+                    }
+                }
         }
     }
     
